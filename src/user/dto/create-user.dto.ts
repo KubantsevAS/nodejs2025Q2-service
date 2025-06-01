7eb62e134cc6eb9cccc4ca3fe2b2abf46da91733
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-06-01 12:03:39 +0300

    feat: add with cli user resource

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  login: string;
+
+  @IsNotEmpty()
+  password: string;
+}
+
+// interface CreateUserDto {
+//     login: string;
+//     password: string;
+//   }
