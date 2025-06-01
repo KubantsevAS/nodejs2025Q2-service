3bce0d326b52f16bba64f9189462736667179dfe
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-06-02 02:17:37 +0300

    fix: validation error

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-      transformOptions: { enableImplicitConversion: true },
+      transformOptions: { enableImplicitConversion: false },
     }),
   );
 
